49be4b1036bc9aaf5c42d3b561038c982491f70c
mm222qy <mm222qy@student.lnu.se>
2014-10-09 12:49:08 +0200

    finished

---
+++ 1.2.rita.med.asterisker/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2.rita.med.asterisker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alength = 25;
+            int blength = 39;
+            int caseswitch = 0;
+            for (int i = 0; i < alength; i++)
+            {
+                caseswitch = i % 3;
+                switch (caseswitch)
+                {
+                    case 0:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        break;
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    default:
+                        break;
+                }
+
+                    if (i > 0)
+                        Console.WriteLine();
+
+                    if (i % 2 == 1)
+                         Console.Write(" "); 
+
+                    for (int j = 0; j < blength; j++)
+                   {
+
+                            Console.Write("* ");
+                    
+
+                   }
+            }
+            Console.ResetColor();
+        }
+    }
+}
